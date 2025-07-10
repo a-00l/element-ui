f55c9fc0d981444713a685b3491b742f25d82e5a
雪海-花 <2010433856@qq.com>
2025-07-10 11:16:04 +0800

    修复Icon图标无法居中对齐的问题

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,54 +1,38 @@
 <template>
-  <button
-    ref="_ref"
-    class="my-button"
-    :class="{
-      [`my-button--${type}`]: type,
-      [`my-button--${size}`]: size,
-      'is-plain': plain,
-      'is-round': round,
-      'is-circle': circle,
-      'is-disabled': disabled,
-      'is-loading': loading,
-    }"
-    :disabled="disabled || loading"
-    :type="nativeType"
-    :autofocus="autofocus"
-  >
-    <Icon
-      icon="spinner"
-      v-if="loading"
-      spin
-    ></Icon>
-    <Icon
-      :icon="icon"
-      v-if="icon"
-    ></Icon>
-    <span>
-      <slot></slot>
-    </span>
+  <button ref="_ref" class="my-button" :class="{
+    [`my-button--${type}`]: type,
+    [`my-button--${size}`]: size,
+    'is-plain': plain,
+    'is-round': round,
+    'is-circle': circle,
+    'is-disabled': disabled,
+    'is-loading': loading,
+  }" :disabled="disabled || loading" :type="nativeType" :autofocus="autofocus">
+    <Icon icon="spinner" v-if="loading" spin></Icon>
+    <Icon :icon="icon" v-if="icon"></Icon>
+    <slot></slot>
   </button>
 </template>
 
 <script setup lang="ts">
-  import Icon from '../Icon/Icon.vue'
-  import { ref } from 'vue'
-  import type { ButtonProps } from './types'
-  defineOptions({
-    name: 'MyButton',
-  })
+import Icon from '../Icon/Icon.vue'
+import { ref } from 'vue'
+import type { ButtonProps } from './types'
+defineOptions({
+  name: 'MyButton',
+})
 
-  // 添加ButtonProps中的默认值
-  const props = withDefaults(defineProps<ButtonProps>(), {
-    nativeType: 'button',
-  })
-  console.log(props)
+// 添加ButtonProps中的默认值
+const props = withDefaults(defineProps<ButtonProps>(), {
+  nativeType: 'button',
+})
+console.log(props)
 
-  // 获取button按钮
-  const _ref = ref<HTMLButtonElement>()
-  defineExpose({
-    ref: _ref,
-  })
+// 获取button按钮
+const _ref = ref<HTMLButtonElement>()
+defineExpose({
+  ref: _ref,
+})
 </script>
 
 <style lang="scss" scoped></style>
